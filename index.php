0cb0ffa210b0cde51388f2b77d33aaa528d209d8
Anthony N <36645319+antnat96@users.noreply.github.com>
2020-02-14 09:26:36 -0500

    CDN for MaterializeCSS

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             <!--Import Google Icon Font-->
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
             <!--Import materialize.css-->
-            <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
             <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
             <!--Let browser know website is optimized for mobile-->
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -35,7 +35,7 @@
 
             <!--JavaScript at end of body for optimized loading-->
             <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-            <script src="js/materialize.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
             <script src="js/init.js"></script>
             <script src="js/functions.js"></script>
         </body>
