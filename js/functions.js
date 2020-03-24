bd8d864ea4aec97b9dd07bbea001a5792d1b0d8d
Anthony N <antnat96@gmail.com>
2020-03-24 17:50:14 -0400

    removing logs

--- js/functions.js
+++ js/functions.js
@@ -76,10 +76,8 @@ $(document).ready(function(){
             },
             type: "POST",
         }).done(function(result){
-            console.log("Done adding note" + result);
             validateSuccess(result, "create");
         }).fail(function(result) {
-            console.log("Failed adding note " + result);
             window.alert("Something went wrong. Please try again later.");
             document.location.href = UrlBuilder("main.php");
         });
@@ -105,9 +103,7 @@ $(document).ready(function(){
     });
 
     function validateSuccess(result, action) {
-        console.log("Validating success");
         if (result != true && result != 1) {
-            console.log("result is not true and is not 1, result is " + result);
             window.alert("Something went wrong. Please try again later.");
             document.location.href = UrlBuilder("main.php");
         }
